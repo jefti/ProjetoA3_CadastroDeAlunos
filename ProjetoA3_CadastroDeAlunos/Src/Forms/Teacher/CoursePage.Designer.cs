7ab9b3e18f1cdb948885ea32dc1c51d9f54b9ae9
Gabri <Gabri@DESKTOP-9Q6NVJR>
2025-05-30 14:05:41 -0300

    Coloque o exibir disciplina.

--- ProjetoA3_CadastroDeAlunos/Src/Forms/Teacher/CoursePage.Designer.cs
+++ ProjetoA3_CadastroDeAlunos/Src/Forms/Teacher/CoursePage.Designer.cs
@@ -28,19 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Curso_dtgrd_cursos = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)Curso_dtgrd_cursos).BeginInit();
             SuspendLayout();
+            // 
+            // Curso_dtgrd_cursos
+            // 
+            Curso_dtgrd_cursos.AllowUserToAddRows = false;
+            Curso_dtgrd_cursos.AllowUserToDeleteRows = false;
+            Curso_dtgrd_cursos.AllowUserToResizeColumns = false;
+            Curso_dtgrd_cursos.AllowUserToResizeRows = false;
+            Curso_dtgrd_cursos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Curso_dtgrd_cursos.Location = new Point(12, 27);
+            Curso_dtgrd_cursos.Name = "Curso_dtgrd_cursos";
+            Curso_dtgrd_cursos.ReadOnly = true;
+            Curso_dtgrd_cursos.Size = new Size(443, 310);
+            Curso_dtgrd_cursos.TabIndex = 1;
+            Curso_dtgrd_cursos.CellContentClick += dataGridView1_CellContentClick;
             // 
             // CoursePage
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(Curso_dtgrd_cursos);
             Name = "CoursePage";
             Text = "CoursePage";
             Load += CoursePage_Load;
+            Controls.SetChildIndex(Curso_dtgrd_cursos, 0);
+            ((System.ComponentModel.ISupportInitialize)Curso_dtgrd_cursos).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView Curso_dtgrd_cursos;
     }
 }
--- ProjetoA3_CadastroDeAlunos/Src/Forms/Teacher/CoursePage.cs
+++ ProjetoA3_CadastroDeAlunos/Src/Forms/Teacher/CoursePage.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using ProjetoA3_CadastroDeAlunos.Src.Repositories;
 
 namespace ProjetoA3_CadastroDeAlunos.Src.Forms.CoursePage
 {
@@ -18,6 +20,49 @@ namespace ProjetoA3_CadastroDeAlunos.Src.Forms.CoursePage
         }
 
         private void CoursePage_Load(object sender, EventArgs e)
+        {
+            using (MySqlConnection connection = Connection.GetConnection())
+            {
+                connection.Open();
+
+                string query = @"
+            SELECT 
+                curso.Nome,
+                categoria.Nome AS categoria,
+                curso.MediaAprovacao,
+                curso.CargaHoraria
+            FROM 
+                curso
+            INNER JOIN 
+                categoria ON categoria.IdCategoria = curso.IdCategoria
+            WHERE 
+                IdFuncionario = @IdFuncionario
+            AND 
+                Status = 1;
+        ";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    // Parâmetro para o IdFuncionario (ajuste conforme necessário)
+                    command.Parameters.AddWithValue("@IdFuncionario", 1);
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        DataTable tabela = new DataTable();
+                        adapter.Fill(tabela);
+
+                        // Preencher o DataGridView com os dados
+                        Curso_dtgrd_cursos.DataSource = tabela;
+
+
+
+
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
